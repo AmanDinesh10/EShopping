1a61b486f74946329108b6880579b6817fbf200a
amandinesh10 <xarmaaaruti@gmail.com>
2023-10-26 10:44:15 +0530

    Created Catalog Project

---
+++ Catalog/Catalog.API/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Catalog.API;
+
+public class Startup
+{
+    public IConfiguration Configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseStaticFiles();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
